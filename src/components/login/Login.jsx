41c96912aa6b49f406c274ded6db387e340c7f43
GENIE <hyunjin117@naver.com>
2024-07-27 00:31:18 +0900

    ✨Feat: Login axios 통신 로직 추가, 검증 완료

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 import {
   LoginContainer,
   LoginBox,
@@ -17,13 +18,37 @@ import {
 import logo from "/src/assets/images/logo.png";
 
 const Login = () => {
-  const [id, setId] = useState("");
+  const [loginId, setloginId] = useState("");
   const [password, setPassword] = useState("");
   const [keepLoggedIn, setKeepLoggedIn] = useState(false);
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
     // 로그인 로직 추가
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/api/users/login`,
+        {
+          loginId,
+          password,
+        }
+      );
+      if (response.status === 200) {
+        // 로그인 성공
+        alert("로그인 성공 : " + response.data);
+        console.log("로그인 성공:", response.data);
+        // 로그인 성공 후 로직 추가 (예: 토큰 저장, 리다이렉트 등)
+      } else if (response.status === 401) {
+        // 로그인 실패 비번 일치 X
+        alert("id 혹은 비밀번호 가 일치하지 않습니다.");
+        console.log("id 혹은 비밀번호 가 일치하지 않습니다.");
+      } else {
+        // 로그인 실패
+        console.log("로그인 실패:", response.data.message);
+      }
+    } catch (error) {
+      console.error("로그인 요청 중 오류 발생:", error);
+    }
   };
 
   return (
@@ -34,8 +59,8 @@ const Login = () => {
           <Input
             type="text"
             placeholder="아이디"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
+            value={loginId}
+            onChange={(e) => setloginId(e.target.value)}
           />
           <Input
             type="password"
