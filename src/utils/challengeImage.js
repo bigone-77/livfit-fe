7784bd3bc7764bcdce44cbd20a036803487d44a9
Seo Hyuneun <hyuneun@skuniv.ac.kr>
2024-08-06 23:14:22 +0900

    :art: 챌린지 이미지 변경

--- src/utils/challengeImage.js
+++ src/utils/challengeImage.js
@@ -1,13 +1,15 @@
 import bigDummy from "@images/challenge/big-dummy.png";
-import challenge from "@images/challenge/challenge.png";
-import challenge1 from "@images/challenge/challenge1.png";
 import running from "@images/challenge/running.png";
+import squat from "@images/challenge/squat.png";
+import lunge from "@images/challenge/lunge.png";
+import exe from "@images/challenge/exe.png";
 
 // ID에 따른 이미지 매핑
 const images = {
   11: running,
-  6: challenge,
-  7: challenge1,
+  12: squat,
+  13: lunge,
+  14: exe
   // 필요한 만큼 추가
 };
 
